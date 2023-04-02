985f0539d7a4e00e311fba72b8eaaca764f71b31
LiamSmith1992 <liamj.smith@hotmail.com>
2023-04-02 13:46:09 -0600

    got coin data

--- bitcoinWatcher.client/src/components/coinTracker.vue
+++ bitcoinWatcher.client/src/components/coinTracker.vue
@@ -6,7 +6,7 @@
   <br>
   <br>
   <div class="">
-    <h3>BTC Current Price</h3>
+    <h3 class="ms-3">BTC Current Price</h3>
     <div class="col-3 d-flex justify-content-center">
       <h1 class="text-primary border text-center ">${{ btc }}</h1>
     </div>
@@ -19,17 +19,18 @@
 import { AppState } from '../AppState';
 import { computed, reactive, onMounted } from 'vue';
 import { logger } from '../utils/Logger.js';
+// import { get } from "http";
 export default {
   setup() {
     onMounted(() => {
       getBitcoinPrice();
-
+      getTrendingCoins()
     })
 
     // const fetch = require('node-fetch');
 
     // Function to fetch current price of Bitcoin
-    const getBitcoinPrice = async () => {
+    async function getBitcoinPrice() {
       try {
         const response = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd');
         const data = await response.json();
@@ -41,9 +42,13 @@ export default {
       }
     }
 
-    // Call the function to fetch current Bitcoin price
-    getBitcoinPrice();
 
+    async function getTrendingCoins() {
+      const res = await fetch('https://api.coingecko.com/api/v3/exchanges/binance/volume_chart?days=1')
+
+      const data = await res.json()
+      logger.log(data)
+    }
 
     return {
       btc: computed(() => AppState.btcPrice)
